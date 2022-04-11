a3f66d86d634b9426953290f5d59e60bda3a3ff0
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-04-12 00:47:03 +0100

    fix generateContracts

--- generateContracts/contracts/mainContract.sol
+++ generateContracts/contracts/mainContract.sol
@@ -20,8 +20,8 @@ contract MainContract is Initializable {
 
     bool public paused; // true = paused; false = unpaused
 
-    event PausedSystem(uint256 Date, bool System_State); // true = paused; false = unpaused
-    event OwnershipTransferred(uint256 Date, address Owner_Address, address New_Owner);
+    event PausedSystem(uint256 Date, bool SystemState); // true = paused; false = unpaused
+    event OwnershipTransferred(uint256 Date, address OwnerAddress, address NewOwner);
 
 
     function _only0wner() private view {
@@ -50,15 +50,12 @@ contract MainContract is Initializable {
     /* ----- MERCHANTs ----- */
     MerchantContract private merchant;
     
-    mapping (uint => address) merchantsAccounts_Addresses;
-    mapping (address => uint256) merchantsAccounts_Balances;
-    mapping (address => string) merchantsAccounts_Names;
-    mapping (address => bool) merchantsAccounts_States; // true = paused; false = unpaused
-    uint256 merchantsAccounts_Counter;
+    mapping (uint => address) merchantsAddresses;
+    uint256 merchantsCounter;
     
-    event PausedMerchant(uint256 Date, address Merchant_Address, bool System_State); // true = paused; false = unpaused
-    event RemovedMerchant(uint256 Date, address Merchant_Address);
-    event CreateMerchantContract(uint256 Date, address Merchant_Address, uint Merchant_Balance, string Merchant_Name, bool Merchant_State);
+    event PausedMerchant(uint256 Date, address MerchantAddress, bool SystemState); // true = paused; false = unpaused
+    event RemovedMerchant(uint256 Date, address MerchantAddress);
+    event CreateMerchantContract(uint256 Date, address MerchantAddress, string MerchantName, bool MerchantState);
     /* --------------------- */
 
 
@@ -67,7 +64,7 @@ contract MainContract is Initializable {
     /*constructor() {
         owner_address = msg.sender;
         paused = false;
-        merchantsAccounts_Counter = 0;
+        merchantsCounter = 0;
     }*/
 
     function initialize(address admin) public initializer {
@@ -78,7 +75,7 @@ contract MainContract is Initializable {
 
         owner_address = admin;
         paused = false;
-        merchantsAccounts_Counter = 0;
+        merchantsCounter = 0;
     }
     
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -99,10 +96,9 @@ contract MainContract is Initializable {
         paused = true;
 
         // Pause All Merchants
-        for(uint i = 0; i < merchantsAccounts_Counter; i++) {
-            merchantsAccounts_States[merchantsAccounts_Addresses[i]] = paused;
+        for(uint i = 0; i < merchantsCounter; i++) {
             merchant.pauseMerchant(msg.sender);
-            console.log("Merchant ", i+1, " Paused: ", merchantsAccounts_Addresses[i]);
+            console.log("Merchant ", i+1, " Paused: ", merchantsAddresses[i]);
             console.log("\n");
         }
         
@@ -114,10 +110,9 @@ contract MainContract is Initializable {
         paused = false;
 
         // Unpause All Merchants
-        for(uint i = 0; i < merchantsAccounts_Counter; i++) {
-            merchantsAccounts_States[merchantsAccounts_Addresses[i]] = paused;
-            merchant.pauseMerchant(msg.sender);
-            console.log("Merchant ", i+1, " Unpaused: ", merchantsAccounts_Addresses[i]);
+        for(uint i = 0; i < merchantsCounter; i++) {
+            merchant.unpauseMerchant(msg.sender);
+            console.log("Merchant ", i+1, " Unpaused: ", merchantsAddresses[i]);
             console.log("\n");
         }
 
@@ -125,9 +120,9 @@ contract MainContract is Initializable {
         emit PausedSystem(block.timestamp, paused);
     }
 
-    function transferOwnership(address New_Owner) public virtual onlyOwner systemState {
-        emit OwnershipTransferred(block.timestamp, owner_address, New_Owner);
-        owner_address = New_Owner;
+    function transferOwnership(address NewOwner) public virtual onlyOwner systemState {
+        emit OwnershipTransferred(block.timestamp, owner_address, NewOwner);
+        owner_address = NewOwner;
         console.log("New Owner address is: ", owner_address);
     }
 
@@ -142,99 +137,51 @@ contract MainContract is Initializable {
 
 
     /* ----- MERCHANTs ----- */
-    function pauseMerchant(address Merchant_Address) public virtual onlyOwner systemState {
-        for(uint i = 0; i < merchantsAccounts_Counter; i++) {
-            if(Merchant_Address == merchantsAccounts_Addresses[i]) {
-                merchantsAccounts_States[Merchant_Address] = true;
-                merchant.pauseMerchant(msg.sender);
-                console.log("Merchant Paused: ", Merchant_Address);
-                return;
-            }
-        }
-        console.log("This Address doesn't exists in the system!");
-        emit PausedMerchant(block.timestamp, Merchant_Address, true);
+    function pauseMerchant(address MerchantAddress) public virtual onlyOwner systemState {
+        merchant.pauseMerchant(msg.sender);
+        console.log("Merchant Paused: ", MerchantAddress);
+        emit PausedMerchant(block.timestamp, MerchantAddress, true);
     }
 
-    function unpauseMerchant(address Merchant_Address) public virtual onlyOwner systemState {
-        for(uint i = 0; i < merchantsAccounts_Counter; i++) {
-            if(Merchant_Address == merchantsAccounts_Addresses[i]) {
-                merchantsAccounts_States[Merchant_Address] = false;
-                merchant.unpauseMerchant(msg.sender);
-                console.log("Merchant Unpaused: ", Merchant_Address);
-                return;
-            }
-        }
-        console.log("This Address doesn't exists in the system!");
-        emit PausedMerchant(block.timestamp, Merchant_Address, false);
+    function unpauseMerchant(address MerchantAddress) public virtual onlyOwner systemState {
+        merchant.unpauseMerchant(msg.sender);
+        console.log("Merchant Unpaused: ", MerchantAddress);
+        emit PausedMerchant(block.timestamp, MerchantAddress, false);
     }
 
-    function removeMerchant(address Merchant_Address) public virtual onlyOwner systemState {
-        for(uint i = 0; i < merchantsAccounts_Counter; i++) {
-            if(Merchant_Address == merchantsAccounts_Addresses[i]) {
-                merchant.pauseMerchant(msg.sender);
-                delete merchantsAccounts_Addresses[i];
+    function removeMerchant(address MerchantAddress) public virtual onlyOwner systemState {
+        for(uint i = 0; i < merchantsCounter; i++) {
+            if(MerchantAddress == merchantsAddresses[i]) {
+                console.log("Merchant Removed: ", MerchantAddress);
+                emit RemovedMerchant(block.timestamp, MerchantAddress);
+                
+                delete merchantsAddresses[i];
                 merchant.deleteMerchant(msg.sender);
-                console.log("Merchant Removed: ", Merchant_Address);
+            }
+        }        
+    }
+
+    function addMerchant(address MerchantAddress, string memory MerchantName) public onlyOwner systemState {
+        require(MerchantAddress != owner_address, "That's the Owner Address!");
+
+        for(uint i = 0; i < merchantsCounter; i++) {
+            if(MerchantAddress == merchantsAddresses[i]) {
+                console.log("This Address already exists in the system!");
                 return;
             }
         }
-        console.log("This Address doesn't exists in the system!");
-        emit RemovedMerchant(block.timestamp, Merchant_Address);
-    }
 
-    function addMerchant(address Merchant_Address, uint256 Merchant_Balance, string memory Merchant_Name) public onlyOwner systemState {
-        require(Merchant_Address != owner_address, "That's the Owner Address!");
-
-        if(verifyMerchantAddress(Merchant_Address) != true) return;
-
-        merchant = new MerchantContract(owner_address, Merchant_Address, Merchant_Balance, Merchant_Name, false);
+        merchant = new MerchantContract(owner_address, MerchantAddress, MerchantName, false);
         
-        merchantsAccounts_Addresses[merchantsAccounts_Counter] = Merchant_Address;
-        merchantsAccounts_Balances[Merchant_Address] = Merchant_Balance;
-        merchantsAccounts_Names[Merchant_Address] = Merchant_Name;
-        merchantsAccounts_States[Merchant_Address] = false;
+        merchantsAddresses[merchantsCounter] = MerchantAddress;
 
         console.log("Created new Merchant!");
-        console.log("Address: ", Merchant_Address);
-        console.log("Address: ", Merchant_Balance);
-        console.log("Name: ", Merchant_Name);
-        console.log("State: Unpaused");
+        console.log("Address: ", MerchantAddress);
+        console.log("Name: ", MerchantName);
         
-        merchantsAccounts_Counter += 1;
+        merchantsCounter += 1;
 
-        emit CreateMerchantContract(block.timestamp, Merchant_Address, Merchant_Balance, Merchant_Name, false);        
-    }
-
-    function verifyMerchantAddress(address Merchant_Address) view private returns(bool) {
-        for(uint i = 0; i < merchantsAccounts_Counter; i++) {
-            if(Merchant_Address == merchantsAccounts_Addresses[i]) {
-                console.log("This Address already exists in the system!");
-                return false;
-            }
-        }
-        console.log("Merchant verified!");
-        return true;
-    }
-
-    function getAllMerchantsInfo() view public onlyOwner {
-        console.log("Total of Merchants: ", merchantsAccounts_Counter);
-        console.log("\n");
-        
-        for(uint i = 0; i < merchantsAccounts_Counter; i++) {
-            console.log("Merchant ", i+1);
-            console.log("Address: ", merchantsAccounts_Addresses[i]);
-            console.log("Balance: ", merchantsAccounts_Balances[merchantsAccounts_Addresses[i]]);
-            console.log("Name: ", merchantsAccounts_Names[merchantsAccounts_Addresses[i]]);
-            console.log("State: ", merchantsAccounts_States[merchantsAccounts_Addresses[i]] == true ? "Paused" : "Unpaused");
-            console.log("--------------");
-        }
-    }
-
-    function balanceOf(address Account_Address) public view onlyOwner systemState returns (uint256) {
-        console.log("Account: ", Account_Address);
-        console.log("Balance: ", merchantsAccounts_Balances[Account_Address]);
-
-        return merchantsAccounts_Balances[Account_Address];
+        emit CreateMerchantContract(block.timestamp, MerchantAddress, MerchantName, false);        
     }
     /* --------------------- */
 }
--- generateContracts/contracts/merchantContract.sol
+++ generateContracts/contracts/merchantContract.sol
@@ -13,8 +13,8 @@ contract MerchantContract {
     address private owner_address;
     bool public paused; // true = paused; false = unpaused
 
-    event PausedMerchant(uint256 Date, address Merchant_Address, bool System_State); // true = paused; false = unpaused
-    event DeletedMerchant(uint256 Date, address Merchant_Address);
+    event PausedMerchant(uint256 Date, address MerchantAddress, bool SystemState); // true = paused; false = unpaused
+    event DeletedMerchant(uint256 Date, address MerchantAddress);
 
     modifier systemState() {
         require(paused == false, "The system is Paused!");
@@ -27,10 +27,9 @@ contract MerchantContract {
     address public merchant_address;
     uint256 public merchant_balance;
     string public merchant_name;
-    //bool public merchant_state;
     
-    event ChangedMerchantAddress(uint256 Date, address Merchant_Address, address New_Merchant_Address);
-    event Refund(uint256 Date, uint ID, string Transaction_Type, address Merchant_Address, address Buyer_Address, uint256 Amount, string Status); // Date | ID | Transaction_Type | From | To | Value | Status
+    event ChangedMerchantAddress(uint256 Date, address MerchantAddress, address NewMerchantAddress);
+    event Refund(uint256 Date, uint ID, string TransactionType, address MerchantAddress, address BuyerAddress, uint256 Amount, string Status); // Date | ID | TransactionType | From | To | Value | Status
 
 
     modifier onlyMerchant() {
@@ -45,37 +44,37 @@ contract MerchantContract {
     //address public buyer_address;
     //uint256 public amount;
 
-    mapping (uint => address) buyersAccounts_Addresses;
-    mapping (address => uint256) buyersAccounts_Amounts;
+    mapping (uint => address) buyersAddresses;
+    mapping (address => uint256) buyersAmounts;
 
     enum State { AWAITING_PAYMENT, WAIT_A_MOMENT, COMPLETE }
     State private currsState;
 
-    mapping (address => State) buyersAccounts_States;
-    uint256 buyersAccounts_Counter;
+    mapping (address => State) buyersStates;
+    uint256 buyersCounter;
 
     using Counters for Counters.Counter;
-    Counters.Counter private Purchase_ID;
+    Counters.Counter private PurchaseID;
     
-    uint256 newPurchase_ID;
+    uint256 newPurchaseID;
     
     
-    // Date | ID | Transaction_Type | From | To | Value | Status
-    event Deposited(uint256 Date, uint ID, string Transaction_Type, address Buyer_Address, address Merchant_Address, uint256 Amount, string Status);
-    event Sell(uint256 Date, uint ID, string Transaction_Type, address Buyer_Address, address Merchant_Address, uint256 Amount, string Status);
+    // Date | ID | TransactionType | From | To | Value | Status
+    event Deposited(uint256 Date, uint ID, string TransactionType, address BuyerAddress, address MerchantAddress, uint256 Amount, string Status);
+    event Sell(uint256 Date, uint ID, string TransactionType, address BuyerAddress, address MerchantAddress, uint256 Amount, string Status);
     /* ------------------ */
 
 
 
     // FUNCTIONS
-    constructor(address Owner_Address, address Merchant_Address, uint256 Merchant_Balance, string memory Merchant_Name, bool Merchant_State) {
-        owner_address = Owner_Address;
-        merchant_address = Merchant_Address;
-        merchant_balance = Merchant_Balance;
-        merchant_name = Merchant_Name;
-        paused = Merchant_State;
+    constructor(address OwnerAddress, address MerchantAddress, string memory MerchantName, bool MerchantState) {
+        owner_address = OwnerAddress;
+        merchant_address = MerchantAddress;
+        merchant_name = MerchantName;
 
-        buyersAccounts_Counter = 0;
+        paused = MerchantState;
+
+        buyersCounter = 0;
     }
 
     /* ----- SYSTEM ----- */
@@ -98,9 +97,9 @@ contract MerchantContract {
     function deleteMerchant(address Address) public {
         require(Address == owner_address, "Only Owner can call this function");
         
-        delete merchant_address;
         console.log("Merchant ", merchant_address, " was deleted!");
         emit DeletedMerchant(block.timestamp, merchant_address);
+        delete merchant_address;
     }
     /* ------------------ */
 
@@ -108,29 +107,29 @@ contract MerchantContract {
 
 
     /* ----- MERCHANT ----- */
-    function changeMerchantAddress(address New_Merchant_Address) public virtual onlyMerchant systemState {
-        emit ChangedMerchantAddress(block.timestamp, merchant_address, New_Merchant_Address);
-        merchant_address = New_Merchant_Address;
-        console.log("New Merchant adress is: ", merchant_address);
+    function changeMerchantAddress(address NewMerchantAddress) public virtual onlyMerchant systemState {
+        emit ChangedMerchantAddress(block.timestamp, merchant_address, NewMerchantAddress);
+        merchant_address = NewMerchantAddress;
+        console.log("New Merchant address is: ", merchant_address);
     }
 
     // REFUNDS
-    function refund(address Buyer_Address, uint256 ID) public payable onlyMerchant systemState {
+    function refund(address BuyerAddress, uint256 ID) public payable onlyMerchant systemState {
         require(msg.value * (1 ether) > 0, "Refund amount should be >0!");
 
-        // buyer_address = Buyer_Address;
+        // buyer_address = BuyerAddress;
         // amount = msg.value;
 
-        payable(Buyer_Address).transfer(msg.value);
+        payable(BuyerAddress).transfer(msg.value);
 
-        // Date | ID | Transaction_Type | From | To | Value | Status
-        emit Refund(block.timestamp, ID, "Refund", merchant_address, Buyer_Address, msg.value, "Complete");
+        // Date | ID | TransactionType | From | To | Value | Status
+        emit Refund(block.timestamp, ID, "Refund", merchant_address, BuyerAddress, msg.value, "Complete");
 
         console.log("Time: ", block.timestamp);
         console.log("Purchase ID: ", ID);
         console.log("Transaction Type: ", "Refund");
         console.log("From: ", merchant_address, "(Merchant)");
-        console.log("To: ", Buyer_Address, "(Buyer)");
+        console.log("To: ", BuyerAddress, "(Buyer)");
         console.log("Value: ", msg.value);
         console.log("Status: ", "Complete");
     }
@@ -142,30 +141,34 @@ contract MerchantContract {
     // ESCROW
     function deposit() public payable systemState {
         require(msg.sender != merchant_address, "Only Buyer can call this funcion");
-        require(buyersAccounts_States[msg.sender] == State.AWAITING_PAYMENT, "Already paid!");
+        //require(buyersStates[msg.sender] == State.AWAITING_PAYMENT, "Already paid!");
         require(msg.value * (1 ether) > 0, "Deposit amount should be >0!");
-
 
         //buyer_address = msg.sender;
         //amount = msg.value;
 
-        if(verifyBuyerAddress(msg.sender) == true) {
-            buyersAccounts_Addresses[buyersAccounts_Counter] = msg.sender;
-            buyersAccounts_Counter += 1;
-            //console.log("Buyer Added!");
+
+        for(uint i = 0; i < buyersCounter; i++) {
+            if(BuyerAddress == buyersAddresses[i]) {
+                console.log("This Address already exists in the system!");
+                return;
+            }
         }
 
-        buyersAccounts_Amounts[msg.sender] = msg.value;
+        buyersAddresses[buyersCounter] = msg.sender;
+        buyersCounter += 1;
+
+        buyersAmounts[msg.sender] = msg.value;
         
-        Purchase_ID.increment();
-        newPurchase_ID = Purchase_ID.current();
+        PurchaseID.increment();
+        newPurchaseID = PurchaseID.current();
 
 
-        // Date | Transaction_Type | From | To | Value | Status
-        emit Deposited(block.timestamp, newPurchase_ID, "Deposit", msg.sender, merchant_address, msg.value, "Complete");
+        // Date | TransactionType | From | To | Value | Status
+        emit Deposited(block.timestamp, newPurchaseID, "Deposit", msg.sender, merchant_address, msg.value, "Complete");
         
         console.log("Time: ", block.timestamp);
-        console.log("Purchase ID: ", newPurchase_ID);
+        console.log("Purchase ID: ", newPurchaseID);
         console.log("Transaction Type: ", "Deposit");
         console.log("From: ", msg.sender, "(Buyer)");
         console.log("To: ", merchant_address, "(Merchant)");
@@ -177,38 +180,27 @@ contract MerchantContract {
         /* ... */
 
         //currsState = State.WAIT_A_MOMENT;
-        buyersAccounts_States[msg.sender] = State.WAIT_A_MOMENT;
+        buyersStates[msg.sender] = State.WAIT_A_MOMENT;
 
-        //transactionSucess(msg.sender, msg.value, newPurchase_ID);
+        //transactionSucess(msg.sender, msg.value, newPurchaseID);
     }
 
-    function verifyBuyerAddress(address Buyer_Address) view private returns(bool) {
-        for(uint i = 0; i < buyersAccounts_Counter; i++) {
-            if(Buyer_Address == buyersAccounts_Addresses[i]) {
-                console.log("This Address already exists in the system!");
-                return false;
-            }
-        }
-        console.log("Buyer verified!");
-        return true;
-    }
-
-    function transactionSucess(address Buyer_Address, uint256 Amount, uint256 ID) private systemState {
-        require(buyersAccounts_States[Buyer_Address] == State.WAIT_A_MOMENT, "Need to make the deposit first!");
+    function transactionSucess(address BuyerAddress, uint256 Amount, uint256 ID) private systemState {
+        require(buyersStates[BuyerAddress] == State.WAIT_A_MOMENT, "Need to make the deposit first!");
         
-        // buyer_address = Buyer_Address;
+        // buyer_address = BuyerAddress;
         // amount = Amount;
 
         payable(merchant_address).transfer(Amount);
-        buyersAccounts_States[Buyer_Address] = State.COMPLETE;
+        buyersStates[BuyerAddress] = State.COMPLETE;
 
-        // Date | ID | Transaction_Type | From | To | Value | Status
-        emit Sell(block.timestamp, ID, "Sell", Buyer_Address, merchant_address, Amount, "Complete");
+        // Date | ID | TransactionType | From | To | Value | Status
+        emit Sell(block.timestamp, ID, "Sell", BuyerAddress, merchant_address, Amount, "Complete");
         
         console.log("Time: ", block.timestamp);
         console.log("Purchase ID: ", ID);
         console.log("Transaction Type: ", "Sell");
-        console.log("From: ", Buyer_Address, "(Buyer)");
+        console.log("From: ", BuyerAddress, "(Buyer)");
         console.log("To: ", merchant_address, "(Merchant)");
         console.log("Value: ", Amount);
         console.log("Status: ", "Complete");

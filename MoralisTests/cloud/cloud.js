45d779d16b43b91c2b6237254228011b14fe0801
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-05-09 23:42:23 +0100

    fix MoralisTests

--- MoralisTests/cloud/cloud.js
+++ MoralisTests/cloud/cloud.js
@@ -16,13 +16,34 @@ Moralis.Cloud.define("cryptopunks", async (request) => {
     return data
 });
 
-Moralis.Cloud.define("transactions", async (request) => {
-    const Timestamp = request.params.date;
+Moralis.Cloud.define("transaction-by-date", async (request) => {
+    const Timestamp = {
+        "__type": "Date",
+        "iso": request.params.date
+    }
 
     const query = new Moralis.Query("PunkTransfers");
     query.equalTo("block_timestamp", Timestamp); // Search the Date sent by query
     const data = await query.find(); // Retrieve All Data
 
     return data
+});
+
+Moralis.Cloud.define("transactions-by-date", async (request) => {
+    const Timestamp = {
+        "__type": "Date",
+        "iso": request.params.date
+    }
+
+    const query = new Moralis.Query("PunkTransfers");
+    query.lessThanOrEqualTo("block_timestamp", Timestamp); // Search all Dates less or equal to the Date sent by query
+    const data = await query.find(); // Retrieve All Data
+    const array = []
+
+    for (let i = 0; i < data.length; ++i) {
+        array[i] = data[i];
+    }
+
+    return array
 });
 

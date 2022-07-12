600200b09fba37acb66f9ee3758d0877324113cb
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-07-12 23:27:13 +0100

    fix contracts

--- generateContracts/contracts/mainContract.sol
+++ generateContracts/contracts/mainContract.sol
@@ -9,6 +9,8 @@ contract MainContract {
     /* ========== SYSTEM ========== */
     address private owner_address;
 
+    // bool teste;
+
     modifier onlyOwner() {
         require(msg.sender == owner_address, "Only Owner can call this function");
         _;
@@ -26,24 +28,33 @@ contract MainContract {
 
     uint merchantsCounter; // count the nº of merchants
     mapping(uint => Merchant) public merchants;
-
+    mapping(address => Merchant) public merchants2;
 
 
     struct Votes {
-        address MerchantContractAddress;
         bool voted; // false: didn't vote; true: already voted
     }
 
-    // msg.sender => Votes
-    mapping(address => Votes) public saveVoters;
+    // msg.sender => (MerchantContractAddress => Votes)
+    mapping(address => mapping(address => Votes)) saveVoters;
 
-    /*struct Votes {
-        address Voter;
-        bool voted; // false: didn't vote; true: already voted
+    // Historic
+    struct MerchantHistoric {
+        uint Sells;
+        uint Refunds;
     }
 
-    // MerchantContractAddress => Votes
-    mapping(address => Votes) public saveVoters;*/
+    // MerchantAddress => MerchantHistoric
+    mapping(address => MerchantHistoric) private merchantHistoric;
+
+
+    struct BuyersHistoric {
+        uint Purchases;
+        uint Cancellations;
+    }
+
+    // BuyerAddress => BuyersHistoric
+    mapping(address => BuyersHistoric) private buyersHistoric;
 
 
 
@@ -61,8 +72,8 @@ contract MainContract {
 
 
     /* ========== MERCHANT_CONTRACTs ========== */
-    function getMerchantAddress(uint MerchantID) public view onlyOwner returns(address) {
-        address merchant_address = merchants[MerchantID].merchantContract.getMerchantAddress();
+    function getMerchantAddress(uint MerchantContractID) public view onlyOwner returns(address) {
+        address merchant_address = merchants[MerchantContractID].merchantContract.getMerchantAddress();
         console.log("Merchant Address: ", merchant_address, "!!");
         return merchant_address;
     }
@@ -75,26 +86,42 @@ contract MainContract {
         console.log("Created new Merchant!");
         console.log("MerchantContract Address: ", address(merchantContract));
         console.log("Address: ", MerchantAddress);
-        console.log("Name: ", MerchantName);
-        
-        freezeWithdrawalsMerchantContract(merchantsCounter);
+        console.log("Name: ", MerchantName);        
 
         emit CreateMerchantContract(merchantsCounter, address(merchantContract), MerchantAddress, MerchantName);
         merchantsCounter++;
     }
 
-    function freezeWithdrawalsMerchantContract(uint MerchantID) public onlyOwner {
-        merchants[MerchantID].merchantContract.pauseMerchant();
+    function approveMerchantContract(uint MerchantContractID) private {
+        merchants[MerchantContractID].merchantContract.approveMerchant();
 
-        console.log("MerchantContract ", address(merchants[MerchantID].merchantContract), " Paused!");
-        emit PausedMerchantContract(address(merchants[MerchantID].merchantContract), true);
+        console.log("MerchantContract ", address(merchants[MerchantContractID].merchantContract), " Approved!");
+        emit ApprovedMerchantContract(address(merchants[MerchantContractID].merchantContract), true);
     }
 
-    function unfreezeWithdrawalsMerchantContract(uint MerchantID) public onlyOwner {
-        merchants[MerchantID].merchantContract.unpauseMerchant();
+    function disapproveMerchantContract(uint MerchantContractID) public onlyOwner {
+        if(merchants[MerchantContractID].status != 2) revert("This address isn't approved!");
 
-        console.log("MerchantContract ", address(merchants[MerchantID].merchantContract), " Unpaused!");
-        emit PausedMerchantContract(address(merchants[MerchantID].merchantContract), false);
+        merchants[MerchantContractID].status = 1;
+        merchants[MerchantContractID].merchantContract.disapproveMerchant();
+
+        address MerchantContractAddress = address(merchants[MerchantContractID].merchantContract);
+        console.log("Merchant Address ", MerchantContractAddress, " has been disapproved!");
+        emit ApprovedMerchantContract(MerchantContractAddress, false);
+    }
+
+    function freezeWithdrawalsMerchantContract(uint MerchantContractID) public onlyOwner {
+        merchants[MerchantContractID].merchantContract.pauseWithdrawals();
+
+        console.log("MerchantContract ", address(merchants[MerchantContractID].merchantContract), " Paused!");
+        emit PausedMerchantContract(address(merchants[MerchantContractID].merchantContract), true);
+    }
+
+    function unfreezeWithdrawalsMerchantContract(uint MerchantContractID) public onlyOwner {
+        merchants[MerchantContractID].merchantContract.unpauseWithdrawals();
+
+        console.log("MerchantContract ", address(merchants[MerchantContractID].merchantContract), " Unpaused!");
+        emit PausedMerchantContract(address(merchants[MerchantContractID].merchantContract), false);
     }
 
     function voteNewMerchantContractApproval(uint MerchantContractID) public {
@@ -103,62 +130,107 @@ contract MainContract {
         if(merchants[MerchantContractID].status == 2) revert("Merchant has already been approved!");
 
         address MerchantContractAddress = address(merchants[MerchantContractID].merchantContract);
-        if(saveVoters[msg.sender].MerchantContractAddress == MerchantContractAddress && saveVoters[msg.sender].voted == true) revert("This address has already voted!");
-        // if(saveVoters[MerchantContractAddress].Voter == msg.sender && saveVoters[MerchantContractAddress].voted == true) revert("This address has already voted!");
 
-        uint voterHistoric = getVoterHistoric(msg.sender);
+        if(saveVoters[msg.sender][MerchantContractAddress].voted == true) revert("This address has already voted in this MerchantContract!");
 
-        if(voterHistoric != 0) {
-            if(voterHistoric <= 10) merchants[MerchantContractID].votes += (voterHistoric*voterHistoric);
+        uint MerchantVotingPower = sqrt(merchantHistoric[msg.sender].Sells) - merchantHistoric[msg.sender].Refunds;
+        uint BuyerVotingPower = sqrt(buyersHistoric[msg.sender].Purchases) - buyersHistoric[msg.sender].Cancellations;
+
+        if(MerchantVotingPower != 0) {
+            if(MerchantVotingPower <= 10) merchants[MerchantContractID].votes += MerchantVotingPower;
             else merchants[MerchantContractID].votes += 100;
         }
-        else merchants[MerchantContractID].votes += 1;
+        // else merchants[MerchantContractID].votes += 1;
 
-        saveVoters[msg.sender].MerchantContractAddress = MerchantContractAddress;
-        saveVoters[msg.sender].voted = true;
-        // saveVoters[MerchantContractAddress].Voter = msg.sender;
-        // saveVoters[MerchantContractAddress].voted = true;
+        if(BuyerVotingPower != 0) {
+            if(BuyerVotingPower <= 10) merchants[MerchantContractID].votes += BuyerVotingPower;
+            else merchants[MerchantContractID].votes += 100;
+        }
+        // else merchants[MerchantContractID].votes += 1;
 
-        console.log("MerchantID: ", MerchantContractID);
+        saveVoters[msg.sender][MerchantContractAddress].voted = true;
+
+        console.log("MerchantContractID: ", MerchantContractID);
         console.log("Merchant Address: ", MerchantContractAddress);
         console.log("Total of votes: ", merchants[MerchantContractID].votes);
 
-        // From | To
+        // Voter | MerchantContract
         emit VoteNewMerchantContractApproval(msg.sender, MerchantContractAddress);
 
-        if(merchants[MerchantContractID].votes > 1000) {
+        if(merchants[MerchantContractID].votes > 5000) {
             merchants[MerchantContractID].status = 2;
             console.log("Merchant Address ", MerchantContractAddress, " has been approved!");
 
-            unfreezeWithdrawalsMerchantContract(MerchantContractID);
+            approveMerchantContract(MerchantContractID);
 
             // NewMerchantContractApproved
             emit NewMerchantContractApproved(MerchantContractAddress);
         }
     }
 
-    function getVoterHistoric(address VoterAddress) private view returns(uint) {
-        uint voterHistoric = 0;
+    function saveHistoric(address MerchantAddress, address BuyerAddress, uint PurchaseStatus) public {
+        /*teste = true;
+        console.log("teste: ", teste);
 
-        for(uint i = 0; i < merchantsCounter; i++) {
-            if(merchants[i].merchantContract.getMerchantHistoric(VoterAddress) != 0) {
-                voterHistoric += merchants[i].merchantContract.getMerchantHistoric(VoterAddress);
-            }
-            else if(merchants[i].merchantContract.getBuyerHistoric(VoterAddress) != 0) {
-                voterHistoric += merchants[i].merchantContract.getBuyerHistoric(VoterAddress);
-            }
+        console.log("Msg.sender 2 is: ", msg.sender);
+        console.log("MainContract address: ", address(this));*/
+
+        address MerchantContractAddress = address(merchants2[msg.sender].merchantContract);
+        uint MerchantContractStatus = merchants2[msg.sender].status;
+
+        if(msg.sender != MerchantContractAddress && MerchantContractStatus != 2) revert("This address isn't approved!");
+
+        if(PurchaseStatus == 0) {
+            // purchase completed
+            merchantHistoric[MerchantAddress].Sells += 1;
+            buyersHistoric[BuyerAddress].Purchases += 1;
+        }
+        else {
+            // purchase refunded
+            merchantHistoric[MerchantAddress].Refunds += 1;
+            buyersHistoric[BuyerAddress].Cancellations += 1;
         }
 
-        return voterHistoric;
+        // MerchantContractAddress | MerchantSells | MerchantRefunds | BuyerPurchases | BuyerCancellations
+        emit SaveHistoric(address(this), merchantHistoric[MerchantAddress].Sells, merchantHistoric[MerchantAddress].Refunds, buyersHistoric[BuyerAddress].Purchases, buyersHistoric[BuyerAddress].Cancellations);
+    }
+
+    function sqrt(uint x) private view returns (uint y) {
+        uint z = (x + 1) / 2;
+        y = x;
+
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+
+        console.log("y = ", y);
     }
 
 
 
+    /* ========== Tests ========== */
+    function MerchantContractCreatePurchase(uint MerchantContractID, uint idPurchase, uint256 purchaseAmount, uint escrowTime) public {
+        merchants[MerchantContractID].merchantContract.createPurchase(idPurchase, purchaseAmount, escrowTime);
+        console.log("MerchantContract ", address(merchants[MerchantContractID].merchantContract), " - purchase created!");
+    }
+
+    function MerchantContractBuy(uint MerchantContractID, uint idPurchase) public {
+        merchants[MerchantContractID].merchantContract.buy(idPurchase);
+        console.log("MerchantContract ", address(merchants[MerchantContractID].merchantContract), " - purchase paid!");
+    }
+
+    function MerchantContractHistoric(uint MerchantContractID, address BuyerAddress, uint purchaseStatus) public {
+        merchants[MerchantContractID].merchantContract.historic(BuyerAddress, purchaseStatus);
+        console.log("MerchantContract ", address(merchants[MerchantContractID].merchantContract), " - historic saved!");
+    }
+
+
     /* ========== EVENTS ========== */
     event CreateMerchantContract(uint ID, address MerchantContractAddress, address MerchantAddress, string MerchantName);
-    event PausedMerchantContract(address MerchantContractAddress, bool SystemState); // true = paused; false = unpaused
-    event ApproveMerchant(address MerchantAddress);
-    event ApproveBuyer(address BuyerAddress);
+    event PausedMerchantContract(address MerchantContractAddress, bool Paused); // true = paused; false = unpaused
+    event ApprovedMerchantContract(address MerchantContractAddress, bool Approved); // true = approved; false = not approved
     event VoteNewMerchantContractApproval(address Voter, address MerchantContractAddress);
     event NewMerchantContractApproved(address MerchantContractAddress);
+    event SaveHistoric(address MerchantContractAddress, uint Sells, uint Refunds, uint Purchases, uint Cancellations);
 }
--- generateContracts/contracts/merchantContract.sol
+++ generateContracts/contracts/merchantContract.sol
@@ -4,13 +4,22 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
+import "./mainContract.sol";
 
 contract MerchantContract is Ownable {
     /* ========== SYSTEM ========== */
-    bool public paused; // true = paused; false = unpaused
+    MainContract mainContract;
 
-    modifier systemState() {
-        require(paused == false, "The system is paused!");
+    bool public approved; // true = approved; false = not approved
+    bool public paused; // true = withdrawals paused; false = withdrawals unpaused
+
+    modifier approvedMerchant() {
+        require(approved == true, "Merchant not approved!");
+        _;
+    }
+
+    modifier pausedWithdrawals() {
+        require(paused == false, "Withdrawals are paused!");
         _;
     }
 
@@ -44,9 +53,22 @@ contract MerchantContract is Ownable {
 
 
     /* ========== HISTORICs ========== */
-    uint private merchantHistoric;
+    struct MerchantHistoric {
+        uint Sells;
+        uint Refunds;
+    }
 
-    mapping(address => uint) private buyersHistoric;
+    // merchant_address => MerchantHistoric
+    mapping(address => MerchantHistoric) private merchantHistoric;
+
+
+    struct BuyersHistoric {
+        uint Purchases;
+        uint Cancellations;
+    }
+
+    // msg.sender => BuyersHistoric
+    mapping(address => BuyersHistoric) private buyersHistoric;
 
 
 
@@ -57,8 +79,7 @@ contract MerchantContract is Ownable {
         total_escrow_amount = 0;
         balance = 0;
         
-        // escrow_time = 259200; // default: 3 days = 259200 seconds
-        paused = false;
+        approved = false;
     }
 
 
@@ -69,43 +90,55 @@ contract MerchantContract is Ownable {
         return merchant_address;
     }
 
-    function pauseMerchant() public onlyOwner {
-        paused = true;
-        console.log("Merchant ", merchant_address, " is paused!");
-        emit PausedMerchant(address(this), paused);
+    function approveMerchant() public onlyOwner {
+        approved = true;
+        console.log("Merchant ", merchant_address, " is approved!");
+        emit ApprovedMerchant(address(this), approved);
     }
 
-    function unpauseMerchant() public onlyOwner {
+    function disapproveMerchant() public onlyOwner {
+        approved = false;
+        console.log("Merchant ", merchant_address, " is disapproved!");
+        emit ApprovedMerchant(address(this), approved);
+    }
+
+    function pauseWithdrawals() public onlyOwner {
+        paused = true;
+        console.log(merchant_address, " --> Merchant withdrawals are paused!");
+        emit PausedWithdrawals(address(this), paused);
+    }
+
+    function unpauseWithdrawals() public onlyOwner {
         paused = false;
-        console.log("Merchant ", merchant_address, " is unpaused!");
-        emit PausedMerchant(address(this), paused);
+        console.log(merchant_address, " --> Merchant withdrawals are unpaused!");
+        emit PausedWithdrawals(address(this), paused);
     }
 
 
 
     /* ========== MERCHANTs ========== */
-    function checkMyAddress() public view onlyMerchant returns(address) {
+    function checkMyAddress() public view onlyMerchant approvedMerchant returns(address) {
         console.log("My address is: ", merchant_address);
         return merchant_address;
     }
 
-    function changeMyAddress(address payable NewAddress) public onlyMerchant {
+    function changeMyAddress(address payable NewAddress) public onlyMerchant approvedMerchant {
         merchant_address = NewAddress;
         console.log("My new address is: ", merchant_address);
         // emit ChangedMyAddress(merchant_address);
     }
 
-    function checkMyEscrowAmount() public view onlyMerchant returns(uint256) {
+    function checkMyEscrowAmount() public view onlyMerchant approvedMerchant returns(uint256) {
         console.log("My EscrowAmount: ", total_escrow_amount);
         return total_escrow_amount;
     }
 
-    function checkMyBalance() public view onlyMerchant returns(uint256) {
+    function checkMyBalance() public view onlyMerchant approvedMerchant returns(uint256) {
         console.log("My Balance: ", balance);
         return balance;
     }
 
-    function createPurchase(uint idPurchase, uint256 purchaseAmount, uint escrowTime) public onlyMerchant {
+    function createPurchase(uint idPurchase, uint256 purchaseAmount, uint escrowTime) public onlyMerchant approvedMerchant {
         // Purchase { uint256 dateF, uint256 amount, uint status, uint256 escrow_amount, uint escrow_time }
         purchases[idPurchase] = Purchase(block.timestamp, purchaseAmount, 1, 0, escrowTime);
 
@@ -116,7 +149,7 @@ contract MerchantContract is Ownable {
         emit CreatePurchase(block.timestamp, purchaseAmount, escrowTime);
     }
 
-    function complete(uint idPurchase) public onlyMerchant {
+    function complete(uint idPurchase) public onlyMerchant approvedMerchant {
         require(purchases[idPurchase].dateF < block.timestamp, "The escrow time of this purchase isn't over yet!");
 
         total_escrow_amount -= purchases[idPurchase].amount;
@@ -131,7 +164,7 @@ contract MerchantContract is Ownable {
         emit Complete(idPurchase);
     }
 
-    function withdrawal() public onlyMerchant systemState {
+    function withdrawal() public onlyMerchant pausedWithdrawals approvedMerchant {
         require(balance > 0, "Balance should be greater than 0!!");
 
         merchant_address.transfer(balance);
@@ -144,7 +177,7 @@ contract MerchantContract is Ownable {
         balance = 0;
     }
 
-    function withdrawalToThisAddress(address payable ThisAddress) public onlyMerchant systemState {
+    /*function withdrawalToThisAddress(address payable ThisAddress) public onlyMerchant pausedWithdrawals approvedMerchant {
         require(balance > 0, "Balance should be greater than 0!!");
 
         ThisAddress.transfer(balance);
@@ -155,18 +188,19 @@ contract MerchantContract is Ownable {
         // From | Amount
         emit Withdrawal(address(this), balance);
         balance = 0;
-    }
+    }*/
 
-    function refund(uint idPurchase, address payable BuyerAddress, uint256 refundAmount) public onlyMerchant systemState {
+    function refund(uint idPurchase, address payable BuyerAddress, uint256 refundAmount) public onlyMerchant pausedWithdrawals approvedMerchant {
         require(refundAmount > 0, "Refund amount should be greater than 0!!");
         require(refundAmount <= purchases[idPurchase].amount, "Refund amount shouldn't be greater than the purchaseAmount!");
 
-        require(purchases[idPurchase].status == 2, "That purchase hasn't yet been paid or has already been refunded!");
+        if(purchases[idPurchase].status == 0) revert("The purchase doesn't exist!"); // By default status is 0
+        if(purchases[idPurchase].status == 1) revert("That purchase hasn't yet been paid!");
+        // if(purchases[idPurchase].status == 2) revert("That purchase has already been paid!");
+        if(purchases[idPurchase].status == 3) revert("That purchase has already been refunded!");
 
-        if(address(this).balance < refundAmount) revert("You don't have enough money in the smart-contract!");
-
-        // If the purchase is still in escrow time, the refund must be 100%
-        if(purchases[idPurchase].dateF > block.timestamp) refundAmount = purchases[idPurchase].amount;
+        // if(address(this).balance < refundAmount) revert("You don't have enough money in the smart-contract!");
+        if(total_escrow_amount+balance < refundAmount) revert("You don't have enough money in the smart-contract!");
 
         if(purchases[idPurchase].escrow_amount >= refundAmount) {
             purchases[idPurchase].escrow_amount -= refundAmount;
@@ -193,7 +227,7 @@ contract MerchantContract is Ownable {
         emit Refund(address(this), BuyerAddress, refundAmount);
     }
 
-    function topUpMyContract() external payable onlyMerchant {
+    function topUpMyContract() external payable onlyMerchant approvedMerchant {
         if(msg.value == 0) revert("Amount should be greater than 0!");
 
         balance += msg.value;
@@ -206,7 +240,7 @@ contract MerchantContract is Ownable {
 
 
     /* ========== BUYERs ========== */
-    function buy(uint idPurchase) external payable /*systemState*/ {
+    function buy(uint idPurchase) external payable approvedMerchant {
         if(msg.value == 0) revert("Amount should be greater than 0!");
 
         if(purchases[idPurchase].status == 0) revert("The purchase doesn't exist!"); // By default status is 0
@@ -214,8 +248,6 @@ contract MerchantContract is Ownable {
         if(purchases[idPurchase].status == 2) revert("The purchase has already been paid!");
         if(purchases[idPurchase].status == 3) revert("The purchase has already been refunded!");
         if(msg.value != purchases[idPurchase].amount) revert("Wrong amount!");
-
-        if(purchases[idPurchase].escrow_time == 0) revert("The escrow time of this purchase is 0!");
 
         total_escrow_amount += msg.value;
         console.log("TotalEscrowAmount: ", total_escrow_amount);
@@ -231,45 +263,47 @@ contract MerchantContract is Ownable {
         emit Buy(idPurchase, purchases[idPurchase].dateF, msg.sender, address(this), msg.value);
     }
 
-
-
     /* ========== HISTORICs ========== */
     function historic(address BuyerAddress, uint purchaseStatus) private {
+        // struct MerchantHistoric { uint Sells; uint Refunds; }
+        // struct BuyersHistoric { uint Purchases; uint Cancellations; }
+
+        // MainContract mainContract;
+
         if(purchaseStatus == 0) {
             // purchase completed
-            merchantHistoric += 1;
-            buyersHistoric[BuyerAddress] += 1;
+            merchantHistoric[merchant_address].Sells += 1;
+            buyersHistoric[BuyerAddress].Purchases += 1;
+
+            // console.log("Msg.sender 1 is: ", msg.sender);
+            // console.log("MerchantContract address: ", address(this));
+
+            mainContract.saveHistoric(merchant_address, BuyerAddress, 0);
         }
         else {
             // purchase refunded
-            merchantHistoric -= 1;
-            buyersHistoric[BuyerAddress] -= 1;
+            merchantHistoric[merchant_address].Refunds += 1;
+            buyersHistoric[BuyerAddress].Cancellations += 1;
+
+            // console.log("Msg.sender 3 is: ", msg.sender);
+            // console.log("MerchantContract address: ", address(this));
+
+            mainContract.saveHistoric(merchant_address, BuyerAddress, 1);
         }
 
-        // MerchantContractAddress | MerchantHistoric | Buyer | BuyerHistoric
-        emit Historic(address(this), merchantHistoric, BuyerAddress, buyersHistoric[BuyerAddress]);
-    }
-
-    function getMerchantHistoric(address MerchantAddress) public view onlyOwner returns(uint) {
-        // if(MerchantAddress != merchant_address) revert("That Merchant address doesn't exist!");
-        if(MerchantAddress != merchant_address) return 0;
-        console.log("Merchant historic is: ", merchantHistoric);
-        return merchantHistoric;
-    }
-
-    function getBuyerHistoric(address BuyerAddress) public view onlyOwner returns(uint) {
-        console.log("Buyer historic is: ", buyersHistoric[BuyerAddress]);
-        return buyersHistoric[BuyerAddress];
+        // MerchantContractAddress | MerchantSells | MerchantRefunds
+        emit Historic(address(this), merchantHistoric[merchant_address].Sells, merchantHistoric[merchant_address].Refunds);
     }
 
 
 
     /* ========== EVENTS ========== */
-    event PausedMerchant(address MerchantContractAddress, bool SystemState); // true = paused; false = unpaused
+    event PausedWithdrawals(address MerchantContractAddress, bool PausedWithdrawals); // true = withdrawals paused; false = withdrawals unpaused
+    event ApprovedMerchant(address MerchantContractAddress, bool ApprovedMerchant); // true = approved; false = not approved
     // event ChangedMyAddress(NewAddress);
     event CreatePurchase(uint256 Date, uint256 Amount, uint EscrowTime);
     event TopUpMyContract(address MerchantContractAddress, uint256 Amount);
-    event Historic(address MerchantContractAddress, uint MerchantHistoric, address BuyerAddress, uint BuyerHistoric);
+    event Historic(address MerchantContractAddress, uint Sells, uint Refunds);
 
     // Purchase Flow
     event Buy(uint PurchaseID, uint256 DateF, address BuyerAddress, address MerchantContractAddress, uint256 Amount);
